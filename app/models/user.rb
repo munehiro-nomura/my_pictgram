dd2a13037a57f38b4bc38943a022f1b8208b06d9
munehiro-nomura <m.nomura1230@gmail.com>
2020-03-08 17:54:46 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
   # 「課題」名前の長さに制限を追加する
-  validates :name, presence: true, length: { maximum: 15}
+  validates :name, presence: true, length: { maximum: 15 }
   # 「課題」　メールアドレスの正規表現を追加
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX}
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
   #　「課題」　パスワードの文字制限、アルファベット、数字の混合のみ可能にしてください。
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])[a-z\d]{8,32}+\z/
-  validates :password, presence: true, length: { minimum: 8, maximum: 32}, format: { with: VALID_PASSWORD_REGEX}
+  validates :password, presence: true, length: { minimum: 8, maximum: 32 }, format: { with: VALID_PASSWORD_REGEX }
   has_secure_password
 end
